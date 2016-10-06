8725acc3ea55e32220c36b77a8412c357250fb43
sfan5 <sfan5@live.de>
2016-10-06 13:32:27 +0200

    Implement json output

--- src/output-json.c
+++ src/output-json.c
@@ -1,18 +1,28 @@
+#include <inttypes.h>
+
 #include "output.h"
+#include "util.h"
 
 static void begin(FILE *f)
 {
-	// TODO
+	(void) f;
 }
 
 static void output_status(FILE *f, uint64_t ts, const uint8_t *addr, uint16_t port, uint8_t ttl, int status)
 {
-	// TODO
+	// {ip: "<ip>", timestamp: <ts>, ports: [{port: <port>, proto: "tcp", status: "<status>", ttl: <ttl>}]},
+	char addrstr[IPV6_STRING_MAX];
+
+	ipv6_string(addrstr, addr);
+	fprintf(f, "{ip: \"%s\", timestamp: %" PRIu64 ", ports: [{port: %u, proto: \"tcp\", status: \"%s\", ttl: %u}]},\n",
+		addrstr, ts, port,
+		status == OUTPUT_STATUS_OPEN ? "open" : "closed", ttl
+	);
 }
 
 static void end(FILE *f)
 {
-	// TODO
+	fprintf(f, "{finished: 1}\n");
 }
 
 const struct outputdef output_json = {

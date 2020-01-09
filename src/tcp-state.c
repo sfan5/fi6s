c660a322b8641e2fceae3f611432c5eb5ceb1505
sfan5 <sfan5@live.de>
2020-01-09 01:54:31 +0100

    Fix accidental low-precision timer in TCP code

--- src/tcp-state.c
+++ src/tcp-state.c
@@ -279,5 +279,5 @@ static inline uint64_t monotonic_ms(void)
 {
 	struct timespec t;
 	clock_gettime(CLOCK_MONOTONIC, &t);
-	return t.tv_sec * 1000 + t.tv_sec / 1000000;
+	return t.tv_sec * 1000 + t.tv_nsec / 1000000;
 }

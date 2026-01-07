6127074802026b6ec1919b2017c4aec3615b93f3
sfan5 <sfan5@live.de>
2026-01-07 13:42:19 +0100

    Extend ci-test2 a bit

--- util/ci-test2.sh
+++ util/ci-test2.sh
@@ -5,7 +5,10 @@ args=(
 	--source-ip   ::1
 )
 
-./fi6s "${args[@]}" --icmp ::1 | tee out.txt
+./fi6s "${args[@]}" --icmp ::1 --output-format binary -o out.bin
+
+./fi6s --readscan out.bin -o out.txt
+
 if ! grep -q "^icmp up 0 ::1 " out.txt; then
 	echo "FAILED!"
 	exit 1

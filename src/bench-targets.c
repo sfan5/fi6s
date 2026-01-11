sfan5/fi6s

commit 09a1be334ecc879cb60d512831760c28daa3ba18

Fix bias with large amounts of targets, of different size

src/target-gen.c
@@ -43,7 +43,8 @@ static int cache_size;
 static FILE *targets_from;
 
 static struct targetstate *targets;
-static unsigned int targets_i, targets_size;
+static unsigned int targets_i /* = used */, targets_size;
+static unsigned int targets_last_i;
 #define REALLOC_TARGETS() \
 	realloc_if_needed((void**) &targets, sizeof(struct targetstate), targets_i, &targets_size)
 
@@ -395,12 +396,13 @@ static void fill_cache(void)
 		return;
 	}
 
-	int unfinished_max = targets_i;
-	while(1) {
-		const int iter_max = unfinished_max;
+	unsigned int unfinished_max = targets_i,
+		iter_min = targets_last_i;
+	do {
+		const unsigned int iter_max = unfinished_max;
 		// update unfinished_max along the way, for the next iteration
 		unfinished_max = 0;
-		for(int i = 0; i < iter_max; i++) {
+		for(unsigned int i = iter_min; i < iter_max; i++) {
 			if(targets[i].done)
 				continue;
 			unfinished_max = i + 1;
@@ -411,14 +413,14 @@ static void fill_cache(void)
 
 			next_addr(&targets[i], &cache[cache_size*16]);
 			cache_size++;
-			if(cache_size == TARGET_RANDOMIZE_SIZE)
-				goto out;
+			if(cache_size == TARGET_RANDOMIZE_SIZE) {
+				// remember index for next time, so we consider all targets evenly
+				targets_last_i = i+1;
+				return;
+			}
 		}
-		if(unfinished_max == 0)
-			goto out;
-	}
-out:
-	return;
+		iter_min = 0;
+	} while(unfinished_max > 0);
 }
 
 static inline uint64_t read_be64(const void *ptr)

commit aeddac82930169d30d6760865dff5ceeb2cf19a0

Fix another stupid bug introduced by 09a1be3

src/bench-targets.c
@@ -57,5 +57,9 @@ int main(int argc, char *argv[])
 	printf("time %.2fs\n", secs);
 	printf("speed %ldk/s\n", (long)(naddr / secs) / 1000);
 
+	if(target_gen_progress() <= 0.999f) {
+		printf("DID NOT FINISH?!\n");
+		return 1;
+	}
 	return 0;
 }

src/target-gen.c
@@ -401,8 +401,9 @@ static void fill_cache(void)
 		iter_min = (targets_last_i + 1) % targets_i;
 	do {
 		const unsigned int iter_max = unfinished_max;
-		// update unfinished_max along the way, for the next iteration
-		unfinished_max = 0;
+		// update unfinished_max along the way, for the next iteration.
+		// take the targets we haven't looked at into account when iterating partially.
+		unfinished_max = iter_min;
 		assert(iter_min < iter_max);
 		for(unsigned int i = iter_min; i < iter_max; i++) {
 			if(targets[i].done)
@@ -423,6 +424,12 @@ static void fill_cache(void)
 		}
 		iter_min = 0;
 	} while(unfinished_max > 0);
+
+	// if we get here, we must be done
+	for(unsigned int i = 0; i < targets_i; i++) {
+		assert(targets[i].done);
+		assert(targets[i].delayed_start == 0);
+	}
 }
 
 static inline uint64_t read_be64(const void *ptr)

util/ci-test4.sh
@@ -112,6 +112,22 @@ done
 
 ##
 
+# differing sizes and bigger than the internal buffer
+printf '%s\n' >in.txt \
+	f00::x f01::xx f02::xxx f03::xxxx
+
+for opt in 1 0; do
+
+	notee=1 try --randomize-hosts $opt --print-hosts @in.txt
+	check_out "^f00::2$"
+	check_out "^f01::24$"
+	check_out "^f02::246$"
+	check_out "^f03::2468$"
+
+done
+
+##
+
 try --icmp 3ffe::/48
 check_out "tremendous amount of time"
 